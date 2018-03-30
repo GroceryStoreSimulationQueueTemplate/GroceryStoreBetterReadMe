d57424f3d3e409be7facc1df1130c0a0529de9be
doxuan994 <doxuancntp@gmail.com>
2018-03-30 16:11:16 -0400

    Create Timer.h

---
+++ Timer.h
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+/******************************************************************
+ * Timer class keeps track of time in this Simulation.
+ * The "minute" time unit is used in this Simulation.
+ *****************************************************************/
+class Timer
+{
+public:
+	typedef int value_type;
+	typedef std::size_t size_type;
+
+	Timer();
+	void setMinutes(value_type m); // Set minutes to the passed value
+	void tick();
+	value_type timeRemaining() const;
+private:
+	value_type minutes;
+};
