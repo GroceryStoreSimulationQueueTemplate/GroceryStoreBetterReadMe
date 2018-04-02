0ef7bde5ed493d9961844191a29dca6ceaa8927b
doxuan994 <doxuancntp@gmail.com>
2018-04-02 14:09:32 -0400

    Update Simulation.h

--- Simulation.h
+++ Simulation.h
@@ -1,5 +1,9 @@
+#ifndef SIMULATION_H
+#define SIMULATION_H
+
+
 #include "Customer.h"
-#include <queue>
+#include <queue> // Provides the queue template class
 
 #include <ctime>
 #include <iostream>
@@ -8,25 +12,29 @@ using namespace std;
 /************************************
 * Simulation.h grocery store.
 ************************************/
-const int MAX = 5; // numbers of lines
+const int MAX = 5; // a number of cashiers
 
 class Simulation
 {
 public:
+        typedef int value_type;
+	typedef std::size_t size_type;
+        
 	Simulation(); // Default Constructor
 	~Simulation(); // Default Destructor.
 
 	void startSimulation();
-	void service(int & busyTimeRemaining, int c);
+	void service(value_type & busyTimeRemaining, value_type cashier);
 	void customersCheckoutAndEnterShortest();
 	void display(ostream & out);
 
 private:
 	// Inputs
-	int lengthOfSimulation;
-	double arrivalRate;
-	int * servicePercent;
-
+        int     *servicePercent; // pointer to an integer
+	int     lengthOfSimulation;
+	double  arrivalRate;
+        int     numCashiers; // numbers of cashiers, set at runtime
+	
 	// Tracking variables
 	int totalServiceTime;
 	int totalCustomerWaitingTime;
@@ -42,10 +50,11 @@ private:
 	// Countdown timer
 	Timer myTimer;
 
-	// Numbers of cashiers, set at runtime
-	int numCashiers;
-
-	// Circular array of queues that store customers
-	Queue<Customer> * allCashiers;
+	// Dynamic array of queues that store customers
+	queue<Customer> *allCashiers; // pointer to an queue of Customer
 };
+
+
+
+#endif /* SIMULATION_H */
 
