921f461b993f16b592245c5ec291351936212844
doxuan994 <doxuancntp@gmail.com>
2018-04-02 14:08:46 -0400

    Update Simulation.cpp

--- Simulation.cpp
+++ Simulation.cpp
@@ -3,72 +3,80 @@
 Simulation::Simulation()
 {
 	// Tracking variables
-	totalServiceTime = 0;
-	totalCustomerWaitingTime = 0;
-	totalCashierIdleTime = 0;
-	totalNumberOfCustomers = 0;
-	customersRemaining = 0;
+	totalServiceTime            = 0;
+	totalCustomerWaitingTime    = 0;
+	totalCashierIdleTime        = 0;
+	totalNumberOfCustomers      = 0;
+	customersRemaining          = 0;
 
 	// Outputs
-	avgCustomerServiceTime = 0;
-	avgCustomerWaitingTime = 0;
-	avgCashierIdleTime = 0;
-
-	// Inputs
-	servicePercent = new int[MAX];
-	allCashiers = new Queue<Customer>[numCashiers];
-
+	avgCustomerServiceTime      = 0;
+	avgCustomerWaitingTime      = 0;
+	avgCashierIdleTime          = 0;
+	
 	// Get inputs for simulation
-
-	cout << "Please enter the average arrival rate of customers in number per minute: " << endl;
+	cout << "Please enter the average arrival rate of customers in number per second: " << endl;
 	cin >> arrivalRate;
 
 	cout << "Please enter the number of cashiers: " << endl;
 	cin >> numCashiers;
+        
+        // Create an instance of all cashiers 
+        allCashiers = new queue<Customer>[numCashiers];
 
-	// TO-DO: Codes broken here
-	// Create the correct number of cashiers
-	for (int i = 0; i < numCashiers; i++)
+	// Correct number of cashiers
+	for ( int i = 0; i < numCashiers; i++ )
 	{
-		allCashiers[i] = *new Queue<Customer>;
+		allCashiers[i] = *new queue<Customer>;
 	}
 
-	cout << "Please enter the length of time to run the simulation in minutes: " << endl;
+        // Get inputs for simulation
+	cout << "Please enter the length of time to run the simulation in seconds: " << endl;
 	cin >> lengthOfSimulation;
+        
+        // Set the countdown timer
+	myTimer.setSeconds( lengthOfSimulation );
+        
+        
+        // Initialize the service percent.
+        // The maximum is 5 cashiers and adds up to 100 percent.
+	servicePercent = new int[MAX]; 
 
+        // Set the service percent to the correct percent which is 20 for each
+        // cashier.
 	int percent = 0;
-	int sum = 0;
-	for (int i = 0; i < MAX; i++)
+	int sum     = 0;
+	for ( int i = 0; i < MAX; i++ )
 	{
 		servicePercent[i] = 20 + sum;
 		sum += 20;
 	}
 
-	while (sum != 100)
+        // Double check to make sure the service percent is added up correctly.
+	while ( sum != 100 )
 	{
-		cout << "Invalid distribution. Must add up to 100 % old sum was: " << sum << endl;
+		cout << "Invalid distribution. Must add up to 100!" << endl; 
 		sum = 0;
-
-		for (int i = 0; i < MAX; i++)
+		for ( int i = 0; i < MAX; i++ )
 		{
-			cout << " <= " << i + 1 << " min. ";   cin >> percent;
+			cout << " <= " << i + 1 << " second. ";   
+                        cin >> percent;
 			servicePercent[i] = percent;
 			sum += percent;
 			cout << "sum: " << sum << endl;
 		}
 	}
 
-	// Set the countdown timer
-	myTimer.setMinutes(lengthOfSimulation);
-
-	// Intialize random number generator
-	long seed = long(time(0));
-	srand(seed);
+	// Initialize random number generator.
+	// Use current time as seed for random generator.
+        // TO-DO:
+        // This is a weak random number generator; it is not useful for security purposes.
+        srand( time( 0 ) );
 }
 
 Simulation::~Simulation()
 {
-	for (int i = 0; i < numCashiers - 1; i++)
+	for( int i = 0; i < numCashiers - 1; i++ )
 	{
 		delete &allCashiers[i];
 	}
@@ -81,46 +89,47 @@ void Simulation::startSimulation()
 // Deletes all dynamically created elements of allCashiers[].
 // Deletes dynamically created member arrays.
 {
-	while (myTimer.timeRemaining() > 0)
+	while ( myTimer.timeRemaining() > 0 )
 	{
-		// Go through each cashier and run the service function which will check
-		// if their customer is ready to check out and do so if able
+            // Go through each cashier and run the service function which will 
+            // check if their customer is ready to check out and do so if able.
 
-		for (int i = 0; i < numCashiers; i++)
+		for ( int i = 0; i < numCashiers; i++ )
 		{
-			if (!allCashiers[i].empty())
+			if ( !allCashiers[i].empty( ) )
 			{
-				int busyTimeLeft = allCashiers[i].front().getServiceTime();
-				service(busyTimeLeft, i);
+                                // Get the remaining service time of the customer in the front of the line.
+				int remainingServiceTime = allCashiers[i].front().getServiceTime();
+				service( remainingServiceTime, i );
 			}
 		}
 	}
 }
 
-void Simulation::service(int & busyTimeRemaining, int c)
+void Simulation::service( Simulation::value_type & busyTimeRemaining, Simulation::value_type cashier )
 // Pass the remaining service time of the customer in the front of the line
 // as well as the cashier of the customer is the line for.
 {
 	// The front customer still has more service time to process.
-	if (busyTimeRemaining > 0)
+	if ( busyTimeRemaining > 0 )
 	{
 		busyTimeRemaining--;
-		allCashiers[c].front().setServiceTime(busyTimeRemaining);
+		allCashiers[cashier].front().setServiceTime( busyTimeRemaining );
 
 		// Add the waiting time of the customers in line to the total.
-		totalCustomerWaitingTime += allCashiers[c].size() - 1;
+		totalCustomerWaitingTime += allCashiers[cashier].size() - 1;
 
 		return;
 	}
 	else
 	{
-		if (!allCashiers[c].empty()) // Check to see if this cashier are available 
+		if ( !allCashiers[cashier].empty() ) // Check to see if this cashier are available 
 		{
-			int arrivalTime = allCashiers[c].front().getArrivalTime();
-			allCashiers[c].pop(); // Done. Let the customer go.
-			if (!allCashiers[c].empty())
+			int arrivalTime = allCashiers[cashier].front().getArrivalTime();
+			allCashiers[cashier].pop(); // Done. Let the customer go.
+			if ( !allCashiers[cashier].empty() )
 			{
-				Customer nextCustomer = allCashiers[c].front();
+				Customer nextCustomer = allCashiers[cashier].front();
 				busyTimeRemaining = nextCustomer.getServiceTime();
 			}
 			customersRemaining--;
@@ -133,14 +142,14 @@ void Simulation::service(int & busyTimeRemaining, int c)
 void Simulation::customersCheckoutAndEnterShortest()
 // Customers enter randomly to check out, and then enter the shortest line.
 {
-	// Activates a new customer proceduce for each arrival per minute.
+	// Activates a new customer procedure for each arrival per minute.
 
 	// -------------------------------------------------------------------
 	// CASE 1: arrivalRate > 1
 	//
 	// -------------------------------------------------------------------
 
-	for (int i = 0; i < arrivalRate; i++)
+	for ( int i = 0; i < arrivalRate; i++ )
 	{
 		// A new customer is ready to get in line.
 		// Create random service time.
@@ -149,7 +158,7 @@ void Simulation::customersCheckoutAndEnterShortest()
 		int serviceTime = 0;
 		int randomServiceTime = servicePercent[serviceTime];
 
-		while (r > randomServiceTime)
+		while ( r > randomServiceTime )
 		{
 			randomServiceTime += servicePercent[serviceTime];
 			serviceTime++;
@@ -167,13 +176,13 @@ void Simulation::customersCheckoutAndEnterShortest()
 		totalNumberOfCustomers++;
 		customersRemaining++;
 
-		int minutes = 9999999;
+		int seconds = 9999999;
 		int shortestLine = 0;
-		for (int i = 0; i < numCashiers; i++)
+		for ( int i = 0; i < numCashiers; i++ )
 		{
-			if (allCashiers[i].size() < minutes)
+			if ( allCashiers[i].size() < seconds )
 			{
-				minutes = allCashiers[i].size();
+				seconds = allCashiers[i].size();
 				shortestLine = i;
 			}
 		}
@@ -188,13 +197,13 @@ void Simulation::customersCheckoutAndEnterShortest()
 	//
 	// -------------------------------------------------------------------
 
-	if (arrivalRate < 1)
+	if ( arrivalRate < 1 )
 	{
 		// Generate a random number between 0 and 99
 		int randomInteger = rand() % 100;
 
 		// Calculates the probability that a customer has arrived
-		if (randomInteger < (100 * arrivalRate))
+		if ( randomInteger < ( 100 * arrivalRate ) )
 		{
 			// A new customer is ready to get in line.
 			// Create random service time.
@@ -203,7 +212,7 @@ void Simulation::customersCheckoutAndEnterShortest()
 			int serviceTime = 0;
 			int randomServiceTime = servicePercent[serviceTime];
 
-			while (r > randomServiceTime)
+			while ( r > randomServiceTime )
 			{
 				randomServiceTime += servicePercent[serviceTime];
 				serviceTime++;
@@ -212,18 +221,18 @@ void Simulation::customersCheckoutAndEnterShortest()
 			totalServiceTime += serviceTime + 1;
 
 			// Customers enter randomly to check out, and then enter the shortest line.
-			Customer newCustomer(myTimer, serviceTime + 1);
+			Customer newCustomer( myTimer, serviceTime + 1 );
 			totalNumberOfCustomers++;
 			customersRemaining++;
 
-			int minutes = 9999999;
+			int seconds = 9999999;
 			int shortestLine = 0;
 
-			for (int i = 0; i < numCashiers; i++)
+			for ( int i = 0; i < numCashiers; i++ )
 			{
-				if (allCashiers[i].size() < minutes)
+				if ( allCashiers[i].size() < seconds )
 				{
-					minutes = allCashiers[i].size();
+					seconds = allCashiers[i].size();
 					shortestLine = i;
 				}
 			}
@@ -236,22 +245,22 @@ void Simulation::customersCheckoutAndEnterShortest()
 
 // Displays the average customer service and wait time 
 // as well as the average cashier serving time.
-void Simulation::display(ostream & out)
+void Simulation::display( ostream & out )
 {
 
-	cout << "\nTotal number of customers: " << totalNumberOfCustomers << endl;
-	cout << "\nTotal service time: " << totalServiceTime << endl;
-	cout << "\nTotal customer waiting time: " << totalCustomerWaitingTime << endl;
+	cout << "\nTotal number of customers: "     << totalNumberOfCustomers << endl;
+	cout << "\nTotal service time: "            << totalServiceTime << endl;
+	cout << "\nTotal customer waiting time: "   << totalCustomerWaitingTime << endl;
 	cout << "\nTotal cashier processing time: " << totalCashierIdleTime << endl;
 
 
-	avgCustomerServiceTime = (double)totalServiceTime / totalNumberOfCustomers;
-	avgCustomerWaitingTime = (double)totalCustomerWaitingTime / totalNumberOfCustomers;
-	avgCashierIdleTime = (double)totalCashierIdleTime / numCashiers;
+	avgCustomerServiceTime = ( double ) totalServiceTime / totalNumberOfCustomers;
+	avgCustomerWaitingTime = ( double ) totalCustomerWaitingTime / totalNumberOfCustomers;
+	avgCashierIdleTime     = ( double ) totalCashierIdleTime / numCashiers;
 
 
 	cout << "\nThe average customer service time was: " << avgCustomerServiceTime << "minutes";
 	cout << "\nThe average customer waiting time was: " << avgCustomerWaitingTime << " minutes";
-	cout << "\nThe average cashier idle time was: " << avgCashierIdleTime << " minutes";
+	cout << "\nThe average cashier idle time was: "     << avgCashierIdleTime << " minutes";
 
 }
