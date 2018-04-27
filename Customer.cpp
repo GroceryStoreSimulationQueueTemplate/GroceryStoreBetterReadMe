d45ccb618af57964de5d67a7ca3ec7fd643a6d6d
doxuan994 <doxuancntp@gmail.com>
2018-04-27 09:56:08 -0400

    Update Customer.cpp

--- Customer.cpp
+++ Customer.cpp
@@ -1,30 +1,28 @@
-#ifndef CUSTOMER_H
-#define CUSTOMER_H
+#include "Customer.h"
 
-#include "Timer.h"
-#include <iostream>
-using namespace std;
-
-/****************************************************************************************
- * Customer class creates a Customer that has a specified time he or she can come to
- * check out and a specified time that a cashier can service this customer.
- ***************************************************************************************/
-class Customer
+Customer::Customer()
 {
-public:
-	typedef int value_type;
-	typedef std::size_t size_type;
+	arrivalTime = 0;
+	serviceTime = 0;
+}
 
-	Customer();
-	Customer(const Timer & arrivalTime, value_type serviceTime);
-	void setServiceTime(value_type serviceTime);
-	value_type getArrivalTime();
-	value_type & getServiceTime();
-private:
-	value_type arrivalTime; // Time the customer arrived
-	value_type serviceTime; // Time the customer is serviced
-};
+Customer::Customer(const Timer & arrival_time, Customer::value_type service_time)
+{
+	arrivalTime = arrival_time.timeRemaining();
+	serviceTime = service_time;
+}
 
+void Customer::setServiceTime(Customer::value_type service_time)
+{
+	serviceTime = service_time;
+}
 
-#endif /* CUSTOMER_H */
+Customer::value_type Customer::getArrivalTime()
+{
+	return arrivalTime;
+}
 
+Customer::value_type & Customer::getServiceTime()
+{
+	return serviceTime;
+}
