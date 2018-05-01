0f8ffd0ccf42d19270a87a3bcd54e8d9b7cc9302
doxuan994 <doxuancntp@gmail.com>
2018-05-01 12:41:52 -0400

    Update Timer.h

--- Timer.h
+++ Timer.h
@@ -4,20 +4,28 @@
 #include <iostream>
 using namespace std;
 
-/******************************************************************
- * Timer class keeps track of time in this Simulation.
- * The "minute" time unit is used in this Simulation.
- *****************************************************************/
+
+/**
+    Timer.h
+    Purpose: Timer class keeps track of time in this Simulation.
+    The "minute" time unit is used in this Simulation.
+
+    @author Xuan Do
+    @author Jason Pagotaisidro
+    @version 1.1 05/01/18 
+*/
+
+
 class Timer
 {
 public:
 	typedef int value_type;
 	typedef std::size_t size_type;
 
-	Timer();
-	void setTime(value_type s); // Set minutes to the passed value
-	void tick();
-	value_type timeRemaining() const;
+	Timer( );
+	void setTime( value_type t ); // Set minutes to the passed value
+	void tick( );
+	value_type timeRemaining( ) const;
 private:
 	value_type minutes;
 };
