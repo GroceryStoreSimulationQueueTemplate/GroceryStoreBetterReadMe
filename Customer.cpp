f57cf302ebfe531aaa43d9f52c8d112969468529
doxuan994 <doxuancntp@gmail.com>
2018-05-01 12:41:24 -0400

    Update Customer.cpp

--- Customer.cpp
+++ Customer.cpp
@@ -1,28 +1,44 @@
 #include "Customer.h"
 
-Customer::Customer()
+/**
+    Customer.cpp
+    Purpose: Customer class creates a Customer that has a specified time he or 
+    she can come to check out and a specified time that a cashier can service 
+    this customer.
+
+    @author Xuan Do
+    @author Jason Pagotaisidro
+    @version 1.1 05/01/18 
+*/
+
+// Default constructor
+Customer::Customer( )
 {
 	arrivalTime = 0;
 	serviceTime = 0;
 }
 
-Customer::Customer(const Timer & arrival_time, Customer::value_type service_time)
+// Parameterized constructor
+Customer::Customer( const Timer & arrival_Time, Customer::value_type service_Time )
 {
-	arrivalTime = arrival_time.timeRemaining();
-	serviceTime = service_time;
+	arrivalTime = arrival_Time.timeRemaining( );
+	serviceTime = service_Time;
 }
 
-void Customer::setServiceTime(Customer::value_type service_time)
+// Set the service time for a customer
+void Customer::setServiceTime( Customer::value_type service_Time )
 {
-	serviceTime = service_time;
+	serviceTime = service_Time;
 }
 
-Customer::value_type Customer::getArrivalTime()
+// Get the arrival time of a customer
+Customer::value_type Customer::getArrivalTime( )
 {
 	return arrivalTime;
 }
 
-Customer::value_type & Customer::getServiceTime()
+// Get the service time of a customer
+Customer::value_type & Customer::getServiceTime( )
 {
 	return serviceTime;
 }
