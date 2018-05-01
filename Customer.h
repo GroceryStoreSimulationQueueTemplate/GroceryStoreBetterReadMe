2f424f1bed4f6ccaa8c4ffebd1a0a9455b4e33d9
doxuan994 <doxuancntp@gmail.com>
2018-05-01 12:40:54 -0400

    Update Customer.h

--- Customer.h
+++ Customer.h
@@ -5,21 +5,30 @@
 #include <iostream>
 using namespace std;
 
-/****************************************************************************************
- * Customer class creates a Customer that has a specified time he or she can come to
- * check out and a specified time that a cashier can service this customer.
- ***************************************************************************************/
+
+/**
+    Customer.h
+    Purpose: Customer class creates a Customer that has a specified time he or 
+    she can come to check out and a specified time that a cashier can service 
+    this customer.
+
+    @author Xuan Do
+    @author Jason Pagotaisidro
+    @version 1.1 05/01/18 
+*/
+
+
 class Customer
 {
 public:
 	typedef int value_type;
 	typedef std::size_t size_type;
 
-	Customer();
-	Customer(const Timer & arrivalTime, value_type serviceTime);
-	void setServiceTime(value_type serviceTime);
-	value_type getArrivalTime();
-	value_type & getServiceTime();
+	Customer( );
+	Customer( const Timer & arrivalTime, value_type serviceTime );
+	void setServiceTime( value_type serviceTime );
+	value_type getArrivalTime( );
+	value_type & getServiceTime( );
 private:
 	value_type arrivalTime; // Time the customer arrived
 	value_type serviceTime; // Time the customer is serviced
